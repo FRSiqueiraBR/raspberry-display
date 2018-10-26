6272033756442bf0bc6bb6d1ac40699b3fd41c37
felipesiqueira <B&atr1zG>
2018-10-25 23:12:49 -0300

    Initial commit

---
+++ display-oled.py
@@ -0,0 +1,68 @@
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+import time
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Raspberry Pi pin configuration:
+RST = None  # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+while True:
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    now = time.strftime('%X %x')
+    text = "Hello World"
+
+    draw.text((x, top), now, font=font, fill=255)
+    draw.text((x, top+8), text, font=font, fill=255)
+
+    disp.image(image)
+    disp.display()
+    time.sleep(.1)
